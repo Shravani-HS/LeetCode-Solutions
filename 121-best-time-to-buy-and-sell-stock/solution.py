c802ca4fabffede95180ec595e512f56979bbf1b
Shravani H S <shravani.hosagadde.satish@gmail.com>
2026-09-10 23:05:48 +0530

    Best Time to Buy and Sell Stock - 2026-09-10T17:35:47.465Z

--- 121-best-time-to-buy-and-sell-stock/solution.py
+++ 121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,17 +1,13 @@
-// 39 ms | 19.1 MB
+// 39 ms | 19 MB
 class Solution(object):
     def maxProfit(self, prices):
         buy = prices[0]
         profit = 0
-
         for price in prices:
             if price < buy:
                 buy = price
-
             current_profit = price - buy
-
             if current_profit > profit:
                 profit = current_profit
-
         return profit
          
