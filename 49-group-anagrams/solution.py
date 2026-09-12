686c8111d8f8d08848ded09295c3f98f6803455b
Shravani H S <shravani.hosagadde.satish@gmail.com>
2026-09-12 23:56:09 +0530

    Group Anagrams - 2026-09-12T18:26:08.363Z

--- 49-group-anagrams/solution.py
+++ 49-group-anagrams/solution.py
@@ -1,14 +1,12 @@
-// 35 ms | 18.3 MB
+// 19 ms | 16.2 MB
 class Solution(object):
     def groupAnagrams(self, strs):
         groups = {}
         for word in strs:
-            count = [0] * 26
-            for char in word:
-                count[ord(char) - ord('a')] += 1
-            key = tuple(count)
+            key = "".join(sorted(word))
             if key not in groups:
                 groups[key] = []
             groups[key].append(word)
+        return list(groups.values())
         return list(groups.values())
         
