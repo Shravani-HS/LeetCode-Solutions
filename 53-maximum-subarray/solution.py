b27125a20f34ff256bc14278338c7ca24f7b589a
Shravani H S <shravani.hosagadde.satish@gmail.com>
2026-09-16 18:54:26 +0530

    Maximum Subarray - 2026-09-16T13:24:27.829Z

---
+++ 53-maximum-subarray/solution.py
@@ -0,0 +1,9 @@
+// 89 ms | 21 MB
+class Solution(object):
+    def maxSubArray(self, nums):
+        current_sum = nums[0]
+        max_sum = nums[0]
+        for num in nums[1:]:
+            current_sum = max(num, current_sum+num)
+            max_sum = max(max_sum, current_sum)
+        return max_sum
