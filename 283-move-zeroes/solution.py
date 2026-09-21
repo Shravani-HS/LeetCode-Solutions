c4a9a8c9cddb051a9dea07d9779b8b9150634804
Shravani H S <shravani.hosagadde.satish@gmail.com>
2026-09-21 20:27:03 +0530

    Move Zeroes - 2026-09-21T14:57:06.324Z

--- 283-move-zeroes/solution.py
+++ 283-move-zeroes/solution.py
@@ -1,9 +1,11 @@
-// 795 ms | 13.3 MB
+// 7 ms | 13.3 MB
 class Solution(object):
     def moveZeroes(self, nums):
+        pos = 0
         for num in nums:
-            if num == 0:
-                nums.remove(0)
-                nums.append(0)
-        return nums
-        
+            if num != 0:
+                nums[pos] = num
+                pos += 1
+        while pos < len(nums):
+            nums[pos] = 0
+            pos += 1
