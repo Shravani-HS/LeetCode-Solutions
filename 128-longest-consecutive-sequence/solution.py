d51240b871a764128fee1a2b9cf217c7c4d17d93
Shravani H S <shravani.hosagadde.satish@gmail.com>
2026-09-22 21:47:30 +0530

    Longest Consecutive Sequence - 2026-09-22T16:17:34.343Z

--- 128-longest-consecutive-sequence/solution.py
+++ 128-longest-consecutive-sequence/solution.py
@@ -1,19 +1,12 @@
-// 69 ms | 27.4 MB
+// 43 ms | 26.9 MB
 class Solution(object):
     def longestConsecutive(self, nums):
-        if len(nums) == 0:
-            return 0
-
-        nums = sorted(set(nums))
-        count = 1
-        longest = 1
-
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1] + 1:
-                count += 1
-            else:
-                count = 1
-
-            longest = max(longest, count)
-
+        num_set = set(nums)
+        longest = 0
+        for num in num_set:
+            if num - 1 not in num_set:
+                length = 1
+                while num + length in num_set:
+                    length += 1
+                longest = max(longest, length)
         return longest
